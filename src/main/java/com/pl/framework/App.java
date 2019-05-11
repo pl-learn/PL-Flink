d67eeeddcb2cb1a1ae207743930057361eb5b687
lipeng <lipeng02@missfresh.cn>
2019-05-11 13:37:00 +0800

    1. init project

---
+++ src/main/java/com/pl/framework/App.java
@@ -0,0 +1,13 @@
+package com.pl.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
